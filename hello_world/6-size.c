31f3ac4c41fdc23e49a2176b02508dee8a25308d
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-13 21:29:41 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,11 +10,11 @@ int main(void)
 
 {
 int a =16;
-printf("Size of char :%d\n",sizeof(char));
-printf("Size of int :%d\n",sizeof(int));
-printf("Size of long int :%d\n",sizeof(long int));
-printf("Size of long long int :%d\n",sizeof(long long int));
-printf("Size of float :%d\n",sizeof(float));
+printf("Size of char:%d\n",sizeof(char));
+printf("Size of int:%d\n",sizeof(int));
+printf("Size of long int:%d\n",sizeof(long int));
+printf("Size of long long in :%d\n",sizeof(long long int));
+printf("Size of float:%d\n",sizeof(float));
 
 return (0)
 
