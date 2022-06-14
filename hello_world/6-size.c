1fc578acff303481a7538489aec5796bd4c4952b
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-13 21:19:11 -0700

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Akways 0 (Success)
+ */
+ 
+int main(void)
+
+{
+int a;
+printf("Size of char :%d\n",sizeof(char));
+printf("Size of int :%d\n",sizeof(int));
+printf("Size of long int :%d\n",sizeof(long int));
+printf("Size of long long int :%d\n",sizeof(long long int));
+printf("Size of float :%d\n",sizeof(float));
+
+return (0)
+
+}
