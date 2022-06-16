52ffa3afc0448cbdcf9fc68a60be94adac00373d
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-15 21:03:58 -0700

    2-print_alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,21 @@
-bbbbb
+#include <stdio.h>
+/**
+ * maain - Entry point
+ *
+ * Return 0 (Succes)
+ */
+int main(void)
+
+{
+
+char ss;
+
+for(ss ='a'; ss <='z'; ss++){
+
+putchar(ss);
+
+}
+
+return (0);
+
+}	
