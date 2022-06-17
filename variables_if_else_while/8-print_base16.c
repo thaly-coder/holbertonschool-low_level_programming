5938fadad7e205bca20ff036c73d11cc7ffbb9d3
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-16 19:58:34 -0700

    8-print_base16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -12,14 +12,13 @@ int main(void)
 	char ss;
 
 	for (ss = '0'; ss <= '9'; ss++)
-	{	
-	putchar(ss);
-	}	
-	for (ss = 'a'; ss <= 'f'; ss++)
-	{	
-	putchar(ss);
-	}	
 	{
+	putchar(ss);
+	}
+	for (ss = 'a'; ss <= 'f'; ss++)
+	{
+	putchar(ss);
+	}
 	putchar('\n');
 	}
 	return (0);
