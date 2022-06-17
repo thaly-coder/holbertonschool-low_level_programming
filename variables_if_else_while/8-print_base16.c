338a2d55ec1ac0684c7d10ee15823d5d231ad7a1
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-16 19:53:42 -0700

    8-print_base16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,1 +1,26 @@
-bbbbb
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char ss;
+
+	for (ss = '0'; ss <= '9'; ss++)
+	{	
+	putchar(ss);
+	}	
+	for (ss = 'a'; ss <= 'f'; ss++)
+	{	
+	putchar(ss);
+	}	
+	{
+	putchar('\n');
+	}
+	return (0);
+}	
