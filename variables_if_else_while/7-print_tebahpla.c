e7990ff8f5b474b72de2d36736db5357915bd45a
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-16 19:30:18 -0700

    7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,21 @@
-nm,n
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Succes)
+ */
+int main(void)
+
+{
+
+	char ss;
+
+
+	for (ss = 'z'; ss >= 'a'; ss--)
+	{
+	putchar(ss);
+	}
+	putchar('\n');
+
+return (0);
+}
