77ea69038211dc008c8c5128c0d1d9d759a521b3
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-16 21:17:41 -0700

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,16 +10,15 @@ int main(void)
 {
 int x;
 
-	for (x = '0'; x  <= '9'; x++)
-        {
-        	putchar(x);
-                if (x != 9)
-        	{
-        	putchar(',');
-		putchar(' ');	
-		}
-	}
-        putchar('\n');
-
-        return (0);
+for (x = '0'; x  <= '9'; x++)
+{
+putchar(x);
+if (x != '9')
+{      	
+putchar(',');
+putchar(' ');	
+}
+}
+putchar('\n');
+return (0);
 }
