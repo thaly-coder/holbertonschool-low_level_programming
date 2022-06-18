618d0253cb099b006933f1baf71f45d410976410
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-17 20:26:40 -0700

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main- Entry point
+ * description: program that prints _putchar, followed by a new line.
+ * Rwturn: (0)
+ */
+
+int main(void)
+
+{
+	char x[9] = "_putchar";
+	int d;
+	
+	for (d = 0; d < 9; d++)
+	{	
+	_putchar(x[9]);
+	}
+	_putchar('\n');
+	
+	return (0);
+
+}
