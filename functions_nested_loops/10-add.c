1cfe10a7decc8a4021e6ccac63d6fc4aa9041a49
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-21 12:11:41 -0700

    10-add.c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,1 +1,16 @@
-mm
+#include "main.h"
+
+/**
+ *add -function that adds two integers and returns the result
+ *@x: integer
+ *@y: integer
+  *Return: vaulue x + z
+ */
+
+int add(int x, int z)
+
+{
+
+	return (x + z);
+
+}
