2e3c56132d302872e6a328e4b7d38028fa2f7b17
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-24 10:13:57 -0500

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -10,14 +10,14 @@
 
 void print_array(int *a, int n)
 
-{	
+{
 	int x = 0;
 
 	for (; x < n; x++)
 	{
 		printf("%d", a[x]);
 		if (x < n - 1)
-			printf (", ");
+			printf(", ");
 	}
 	putchar('\n');
 
