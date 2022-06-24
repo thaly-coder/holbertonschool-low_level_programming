6ebb5331250992be022cd59dcce8bfeb8137146c
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-23 22:48:20 -0500

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,1 +1,21 @@
-mmmmmm
+#include "main.h"
+
+/**
+ *_puts - function that prints a string
+ *@str: input string
+ *Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int c = 0;
+
+	while (str[c] != '\0')
+	{
+		_putchar(str[c]);
+		c++;
+	}
+	_putchar('\n');
+}	
+		
+
