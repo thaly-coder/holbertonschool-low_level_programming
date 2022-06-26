447b5cf7c6520ab2446b69b3abbf5486349654fe
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-25 19:05:05 -0500

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *_strncpy - funcction that copy strings
+ *@dest: input string
+ *@src: input string
+ *@n: input integer
+ *Return: porinter to resulting string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int srclen = 0, i = 0;
+	char *tmp = dest, *str = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+	srclen++;
+
+	if (n > srclen)
+		n = srclen;
+	src = str;
+
+	for (; i < n ; i++)
+		*dest++ = *src++;
+	return (tmp);
+}
