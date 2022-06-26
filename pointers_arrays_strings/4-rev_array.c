cfd2ca08cfe7054c981e8610cc8a7e4917313829
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-25 21:34:39 -0500

    3-strcmp.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *reverse_array - a function that reverse an array
+ *@a: input array
+ *@n: elements of array
+ *Return : nothing
+ */
+
+void reverse_array(int *a, int n);
+{
+	int i = 0;
+	int aux;
+
+	while (i < n --)
+	{
+		aux = a[i];
+		a[i++] = a[n];
+		a[n] = aux;
+	}
+}
