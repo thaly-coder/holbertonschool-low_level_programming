08eaa462becd1ce997770167efbb1a22e9ae8e9c
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-28 18:02:39 -0500

    4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -12,13 +12,14 @@
 char *_strpbrk(char *s, char *accept)
 {
 	char *start = accept;
+
 	while (*s)
 
 	{
 		while (*accept)
 		{
 
-		if ( *s == *accept)
+		if (*s == *accept)
 			return (s);
 		accept++;
 		}
