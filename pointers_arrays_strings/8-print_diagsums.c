08afc997f489b83b09cadf61e9f73c6cfbf10e4c
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-29 16:52:32 -0500

    8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -14,13 +14,14 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < size; j++)
-
+		for (j = 0; j < size; j++)
+		{
 			if (i == j)
 			x += a[i,j];
 
 			if ((i + j) == (size - 1))
 			y += a[i,j];
+		}	
 	}
 	
 }
