045b47e4d3706a6dffd78c6b3297d92e05b9bd0d
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-29 12:04:45 -0500

    9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -10,14 +10,14 @@
 
 char *_strcpy(char *dest, char *src) 
 {
-	
-	     int x = 0;
 
-	     while (src[x] != '\0')			     
-             {
-	     	dest[x] = src[x];
-		       x++;	
-	     }
+	int x = 0;
+
+	while (src[x] != '\0')
+	{
+		dest[x] = src[x];
+			x++;
+	}
 
 	dest[x] = src[x];
 
