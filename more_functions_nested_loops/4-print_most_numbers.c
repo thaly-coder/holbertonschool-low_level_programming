a4656aaf159c38eefe53be1b21604adea3ceb525
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-30 00:34:22 -0500

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_more_numbers - function that prints the numbers,
+ * print_most_numbers - function that prints the numbers,
  * from 0 to 9, not print 2 and 4
  * Return: 0
  */
