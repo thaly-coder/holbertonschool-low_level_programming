7efc18b1fd6a2e78e324ca0c9a9d81c7438386a2
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-30 14:56:49 -0500

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -9,15 +9,16 @@
 void print_square(int size)
 {
 	int i, j;
+
 	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
-			  	_putchar('#');
+				_putchar('#');
 			_putchar('\n');
 		}
 	}
 	else
 		_putchar('\n');
-}	
+}
