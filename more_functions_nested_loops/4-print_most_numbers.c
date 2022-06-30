473e929df8218128b030ce409fce8d4979f35f36
thaly-coder <nathalyq.caicedo@gmail.com>
2022-06-30 00:19:51 -0500

    4-print_most_numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints the numbers,
+ * from 0 to 9, not print 2 and 4
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i, '0';
+
+	for (; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+	}
+	_putchar('\n');
+}
